d2f686003cc72d2231c450e874435088e00f33e0
Rowan Crawford <wombleton@gmail.com>
2017-09-25 18:19:13 +1300

    Create Link

--- src/components/AdventurersIndex.jsx
+++ src/components/AdventurersIndex.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class AdventurersIndex extends React.Component {
   render() {
     return (
-      <ul>
-        <li>List of adventurers</li>
-      </ul>
+      <div>
+        <div>
+          <Link to="/adventurers/new">Create New</Link>
+        </div>
+        <ul>
+          <li>List of adventurers</li>
+        </ul>
+      </div>
     )
   }
 }
